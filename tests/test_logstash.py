bc6e652c57acfdc47f5d99ab1364251fb1eb5515
segulee <segulee@gmail.com>
2021-02-05 03:29:08 +0900

    git test code init

---
+++ tests/test_logstash.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+class LogstashLoggerTestCase(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_something(self):
+        pass
+    
+
